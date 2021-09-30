ea0a3f055e0e97874a3e681e9e3fdd6049d5862d
schlittjanoshenrik <43171864+schlittjanoshenrik@users.noreply.github.com>
2021-09-30 12:45:45 +0200

    Gyök

---
+++ 20200902/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20200902
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
